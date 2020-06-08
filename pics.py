6cdd2a987f0031d9fb6463c77dca294ae566f3d6
YP_Wang <wangyp@smail.nju.edu.cn>
2020-06-08 10:35:52 +0800

    add pic memory

--- pics.py
+++ pics.py
@@ -1,8 +1,9 @@
 import os
 import json
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from utils import survey
+from utils import survey, algs, datasets, algorithms
 plt.style.use("ggplot")
 
 
@@ -24,7 +25,6 @@ def pic_epochs(datasets, algorithms):
 
 
 def pic_averge_time():
-    from utils import datasets
     dir_name = "epochs/"
     all_data = {}
     for data in datasets:
@@ -112,7 +112,7 @@ def pic_operators():
         del ax
 
 
-if __name__ == '__main__':
+def run_stages():
     dicts = {
         'stages': ['Forward', 'Backward', 'Eval'],
         'layers': ['Layer0', 'Layer1', 'Loss', 'Other'],
@@ -130,3 +130,47 @@ if __name__ == '__main__':
     # for label in dicts.keys():
     #     pic_stages(label, dicts[label], algorithms)
     pic_stages('layers', dicts['layers'], algorithms)
+
+
+def pic_memory():
+    time_labels = ['Data\nLoad', 'Warm\nUp', 'Forward\nLayer0', 'Forward\nLayer1', 'Forward\nEnd', 'Backward\nEnd',
+                   'Eval\nLayer0', 'Eval\nLayer1', 'Eval\nEnd']
+
+    dir_name = "C:\\Users\\hikk\\Desktop\\config_exp\\dir_json\\"
+    for data in datasets:
+        allocated_current = {}
+        for alg in algs:
+            file_path = dir_name + 'config0_' + alg + '_' + data + '.json'
+            if not os.path.exists(file_path):
+                continue
+            with open(file_path) as f:
+                res = json.load(f)
+                dataload_end = np.array(res['forward_start'][0])
+                warmup_end = np.array(res['forward_start'][1:]).mean(axis=0)
+                layer0_forward = np.array(res['layer0'][1::2]).mean(axis=0)
+                layer0_eval = np.array(res['layer0'][2::2]).mean(axis=0)
+                layer1_forward = np.array(res['layer1'][1::2]).mean(axis=0)
+                layer1_eval = np.array(res['layer1'][2::2]).mean(axis=0)
+                forward_end = np.array(res['forward_end'][1:]).mean(axis=0)
+                backward_end = np.array(res['backward_end'][1:]).mean(axis=0)
+                eval_end = np.array(res['eval_end']).mean(axis=0)
+                all_data = np.array([dataload_end, warmup_end, layer0_forward, layer1_forward, forward_end,
+                                     backward_end, layer0_eval, layer1_eval, eval_end])
+                all_data /= 1024 * 1024
+                all_data = all_data.T  # 得到所有的數據
+
+                allocated_current[algorithms[alg]] = all_data[0]
+
+        allocated_current = pd.DataFrame(allocated_current, index=time_labels)
+        ax = plt.gca()
+        ax.set_ylabel("GPU Memory Usage (MB)")
+        ax.set_title(data)
+        colors = 'rgbm'
+        markers = 'oD^s'
+        lines = ['-', '--', '-.', ':']
+        for i, c in enumerate(allocated_current.columns):
+            allocated_current[c].plot(ax=ax, color=colors[i], marker=markers[i], linestyle=lines[i], label=c, rot=45)
+        ax.legend()
+        plt.show()
+        ax.get_figure().savefig("memory/" + data + ".png")
+
--- test/debugs.py
+++ test/debugs.py
@@ -1,79 +1,51 @@
-from utils import datasets, algorithms, survey, algs
-import json
 import os
-import numpy as np
+import json
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
+from utils import algs, datasets, algorithms
 
+plt.style.use("ggplot")
 
-def fun():
-    for alg in ['gcn', 'ggnn', 'gat', 'gaan']:
-        dir_path = '../operators/' + alg + '/'
-        df = {}
-        for data in datasets:
-            file_path = dir_path + data + ".json"
-            if not os.path.exists(file_path):
-                continue
-            print(file_path)
-            with open(file_path) as f:
-                operators = json.load(f)
-                sum_time = sum(operators.values())
-                df[data] = [operators[op] for op in columns[alg]]
-                others_time = sum_time - sum(df[data])
-                df[data].append(others_time)
-        df = pd.DataFrame(df)
-        df.to_csv("../operators/" + alg + ".csv")
-        labels = df.columns
-        data = 100 * df.values / df.values.sum(axis=0)
+time_labels = ['Data\nLoad', 'Warm\nUp', 'Forward\nLayer0', 'Forward\nLayer1', 'Forward\nEnd', 'Backward\nEnd', 'Eval\nLayer0', 'Eval\nLayer1', 'Eval\nEnd']
+dir_name = "C:\\Users\\hikk\\Desktop\\config_exp\\dir_json\\"
 
-        fig, ax = survey(labels, data.T, columns[alg] + ['others'])
-        ax.set_title(algorithms[alg], loc="right")
-        ax.set_xlabel("%")
-        fig.savefig("../operators/" + alg + ".png")
-        plt.show()
-        del ax
-
-
-for alg in algs:
-    dir_path = '../operators/' + alg + '/'
-    all_percent_ops = {} # 总的percent ops
-    res = {}
-    cnt = 0
-    for data in datasets:
-        file_path = dir_path + data + '.json'
+for data in datasets:
+    allocated_current = {}
+    for alg in algs:
+        file_path = dir_name + 'config0_' + alg + '_' + data + '.json'
         if not os.path.exists(file_path):
             continue
-        cnt += 1
         with open(file_path) as f:
-            ops = json.load(f)
-            s = sum(ops.values())
-            percent_ops = {k: 100.0 * ops[k] / s for k in ops.keys()} # 先算比例
-            all_percent_ops[data] = percent_ops
-            if res == {}:
-                res = percent_ops.copy()
-            else:
-                for k in res.keys():
-                    res[k] += percent_ops[k]
+            res = json.load(f)
+            dataload_end = np.array(res['forward_start'][0])
+            warmup_end = np.array(res['forward_start'][1:]).mean(axis=0)
+            layer0_forward = np.array(res['layer0'][1::2]).mean(axis=0)
+            layer0_eval = np.array(res['layer0'][2::2]).mean(axis=0)
+            layer1_forward = np.array(res['layer1'][1::2]).mean(axis=0)
+            layer1_eval = np.array(res['layer1'][2::2]).mean(axis=0)
+            forward_end = np.array(res['forward_end'][1:]).mean(axis=0)
+            backward_end = np.array(res['backward_end'][1:]).mean(axis=0)
+            eval_end = np.array(res['eval_end']).mean(axis=0)
+            all_data = np.array([dataload_end, warmup_end, layer0_forward, layer1_forward, forward_end,
+                                 backward_end, layer0_eval, layer1_eval, eval_end])
+            all_data /= 1024 * 1024
+            all_data = all_data.T # 得到所有的數據
 
-    res = {k: res[k] / cnt for k in res.keys()} # 对数据集求平均
-    res_sort = sorted(res.items(), key=lambda x: x[1], reverse=True) # 排序，选择topk算子
-    columns = [i[0] for i in res_sort[:5]]
+            allocated_current[algorithms[alg]] = all_data[0]
 
-    df = {} # 获取实际百分比
-    for k in all_percent_ops.keys():
-        df[k] = []
-        for c in columns:
-            df[k].append(all_percent_ops[k][c])
-        df[k].append(100 - sum(df[k]))
-
-    df = pd.DataFrame(df)
-    df.to_csv("../operators/" + alg + ".csv")
-    columns.append('others')
-
-    fig, ax = survey(df.columns, df.values.T, columns)
-    ax.set_title(algorithms[alg], loc="right")
-    ax.set_xlabel("%")
-    fig.savefig("../operators/" + alg + ".png")
+    allocated_current = pd.DataFrame(allocated_current, index=time_labels)
+    ax = plt.gca()
+    ax.set_ylabel("GPU Memory Usage (MB)")
+    ax.set_title(data)
+    colors = 'rgbm'
+    markers = 'oD^s'
+    lines = ['-', '--', '-.', ':']
+    for i, c in enumerate(allocated_current.columns):
+        allocated_current[c].plot(ax=ax, color=colors[i], marker=markers[i], linestyle=lines[i], label=c, rot=45)
+    ax.legend()
     plt.show()
-    del ax
+    ax.get_figure().savefig("../memory/" + data + ".png")
+
+
 
