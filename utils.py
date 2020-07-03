19eb88ab6a65755c1dac468a730939a181981d03
AuF <wangyp@smail.nju.edu.cn>
2020-07-03 21:40:31 +0800

    增加

--- utils.py
+++ utils.py
@@ -24,6 +24,7 @@ sampling_modes = {
 
 # 这里将config实验除外，因为只涉及到两个参数
 # hds_heads_exp 参数
+datasets = ['amazon-photo', 'pubmed', 'amazon-computers', 'coauthor-physics', 'flickr', 'com-amazon']
 dir_out = r"hds_heads_exp"
 dir_name = r"/home/wangzhaokang/wangyunpan/gnns-project/pyg-gnns/hidden_dims_exp/dir_head_sqlite"
 dir_memory = r"/home/wangzhaokang/wangyunpan/gnns-project/pyg-gnns/hidden_dims_exp/dir_head_json"
